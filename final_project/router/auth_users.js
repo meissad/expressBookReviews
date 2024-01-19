f9fbc73b5f5a0608248892e00ec0c7e198e1cc13
meissad <sameiidia1@gmail.com>
2024-01-19 17:56:33 -0500

    reviews doneclear

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -64,18 +64,46 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
   const isbn = req.params.isbn;
   const usr = req.session.username;
   let review = req.body.review;
-  let keys = Object.keys(books[isbn].reviews);
-  /*let filtered_key = keys.filter(key =>{
-    return (key === usr)
-  });
-  if(filtered_key.length > 0){
-    books[isbn].reviews[usr] = review;
+  let filtered_owner = books[isbn].reviews.filter(review =>{
+    return (review.owner === usr)
+  })
+  if( review ){
+    if(filtered_owner.length > 0){
+        books[isbn].reviews.forEach(thereview =>{
+            if(thereview.owner === usr){
+                thereview.content = review;
+                return (res.status(200).json({"Review modified": thereview},null,4))
+            }
+        });
+    }else{
+        books[isbn].reviews.push({owner: usr, content: review});
+        return (res.status(200).json({"Review added": {"owner": usr, "content": review}},null,4))
+    }
   }else{
-    books[isbn].reviews[usr] = review;
-  }*/
-  return res.status(200).send(books[isbn].reviews);
+  return res.status(208).json({message: "No review to add! Try again"});}
+});
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    let usr = req.session.username
+    let isbn = req.params.isbn;
+    let filtered_owner = books[isbn].reviews.filter(review =>{
+        return (review.owner === usr)
+      });
+    if(filtered_owner.length > 0){
+        let myreviews = books[isbn].reviews; 
+        myreviews = myreviews.filter(review =>{
+            return(review.owner != usr)
+        });
+        books[isbn].reviews = myreviews;
+
+        return ( res.status(200).json({"One review has been deleted: Updated reviews here ->": books[isbn].reviews}, null,4) )
+        
+
+    }else{
+        return ( res.status(208).json({message: "You have no review here"}) )
+    }
 });
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
-module.exports.users = users;
+module.exports.users = users; 
