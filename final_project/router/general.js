50aeeec0ba37ecee2f5429d80c0f4d119d56e98d
meissad <sameiidia1@gmail.com>
2024-01-20 10:21:03 -0500

    All tasks completed

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -24,39 +24,85 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).send(JSON.stringify({books},null,4));
+  new Promise((resolve, reject) =>{
+    if(books){
+        resolve(JSON.stringify({books},null,4));}
+    else{
+        reject("The library is empty");
+    }
+  })
+  .then(result =>{
+    res.status(300).send(result);
+  })
+  .catch(error =>{
+    reject(error)
+  })
+   
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
   const isbn = req.params.isbn;
-  return res.status(300).json(books[isbn]);
+  new Promise((resolve,reject) =>{
+    if(isbn){
+        resolve(books[isbn])
+    }else{
+        reject("ISBN not valid")
+    }
+  })
+  .then(result =>{
+    res.status(300).json(result);
+  })
+  .catch(error =>{
+    reject(error)
+  })
+  
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
   const author = req.params.author;
+  new Promise((resolve,reject) =>{
   let keys = Object.keys(books);
   let filtered_author = keys.filter(key => books[key].author === author);
   let newbooks = [];
   filtered_author.forEach(key =>{
     newbooks.push(books[key]);
   });
-  return res.status(300).send(newbooks, null, 4);
+  resolve(newbooks,null,4)
+  })
+  .then(result =>{
+    res.status(300).send(result);
+  })
+  .catch(error =>{
+    reject(error)
+  })
+
+  
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  let title = req.params.title;
-  let filtered_title = Object.keys(books).filter(key => books[key].title === title);
-  let booktitle = [];
-  filtered_title.forEach(key =>{
-    booktitle.push(books[key]);
-  });
-  return res.status(300).send(booktitle, null, 4);
+  new Promise((resolve,reject)=>{
+    let title = req.params.title;
+    if(title){
+        let filtered_title = Object.keys(books).filter(key => books[key].title === title);
+        let booktitle = [];
+        filtered_title.forEach(key =>{
+          booktitle.push(books[key]);
+        });
+        if(booktitle.length > 0){
+            resolve(booktitle,null,4)
+        }else{
+            reject("No book for this title")
+        }
+    };
+  })
+  .then(result =>{res.status(300).send(result)})
+  .catch(error =>{res.status(500).send(error)})
 });
 
 //  Get book review
